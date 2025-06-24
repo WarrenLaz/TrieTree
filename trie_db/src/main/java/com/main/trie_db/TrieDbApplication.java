21e1d1f3adcf32a4a6c24b1c6e1a5f2f3d176be7
Warren Lazarraga <79947453+WarrenLaz@users.noreply.github.com>
2025-06-24 19:32:14 -0400

    added springboot

---
+++ trie_db/src/main/java/com/main/trie_db/TrieDbApplication.java
@@ -0,0 +1,13 @@
+package com.main.trie_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrieDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrieDbApplication.class, args);
+	}
+
+}
---
+++ trie_db/src/test/java/com/main/trie_db/TrieDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.trie_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrieDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
